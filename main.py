440c956ce26ced3279b6a2a4c58ee7afe6810d02
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-31 21:29:56 -0300

    Implementa streamlit

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import streamlit as st
 from sorteio import sorteio
 
-nomes = input('Quais nomes gostaria de incluir no sorteio (informe separando por virgula): ')
-sorteado = sorteio(nomes.split(','))
-print(sorteado)
+
+with st.form("my_form"):
+    st.write("Formulário de sorteio de organização HH!")
+    names = st.text_input(
+        'Informe os participantes do sorteio: ',
+        placeholder='Separe os nomes dos participantes por vírgula.'
+        )
+    submit = st.form_submit_button('Enviar')
+
+    if submit:
+        sorteado = sorteio(names.split(','))
+        st.write(f'O nome sorteado foi: {sorteado}')
